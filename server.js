c555a7e71ce35106376d1639b45e17908ad62999
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-05-03 09:18:27 -0600

    Server created

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3005;
+
+app.get('/', (req, res) =>
+res.json('Hello from server'))
+
+app.listen(PORT, () =>
+    console.log(`Server is running at hhtp://localhost:${PORT}`));
