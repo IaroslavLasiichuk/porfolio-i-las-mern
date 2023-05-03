3075202fb4eb9a146496f352c430de9c3cedb5da
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-05-03 12:47:31 -0600

    Adds pages and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import React from "react"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
@@ -18,20 +14,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <ul>
+        {/* <ul>
         {data ? (
         <ul>
           {data.map((customer) => (
@@ -41,11 +24,7 @@ function App() {
       ) : (
         <p>Loading...</p>
       )}
-      </ul>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </ul> */}
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Navbar from './pages/Navbar.jsx'
+import Hero from './pages/Hero.jsx'
+import Contact from './pages/Contact.jsx'
+import Footer from './pages/Footer.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Navbar />
+    <Hero />
+    <Contact />
+    <Footer />
   </React.StrictMode>,
 )
