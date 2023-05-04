7e55f0cd4cc360899dea1d193a76664126f318e5
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-05-04 17:04:20 -0600

    Concurrently run server i client side

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import App from './App.jsx'
 import Navbar from './pages/Navbar.jsx'
 import Hero from './pages/Hero.jsx'
