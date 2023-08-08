80806a0def88834b9bb09f9c51d4e6e890a0080b
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-08-08 16:54:43 -0600

    Sitemap generator and robots.txt on server side and domain changed

---
+++ server/sitemap_generator.js
@@ -0,0 +1,40 @@
+const { SitemapStream, streamToPromise } = require('sitemap');
+const { Readable } = require('stream');
+let sitemap;
+
+const generate_sitemap = async (req, res, next) => {
+
+  res.header('Content-Type', 'application/xml');
+
+  if (sitemap) return res.status(200).send(sitemap); // If we have a cached entry send it
+
+  let changefreq = 'weekly';
+
+  try {
+
+    let links = [
+      { url: '', changefreq, priority: 1 },
+      { url: 'portfolio', changefreq, priority: 0.9 },
+      { url: 'blog', changefreq },
+      { url: 'contact', changefreq },
+      { url: 'posts/:postId', changefreq },
+      { url: 'registration', changefreq },
+      { url: 'dashboard', changefreq },
+      { url: 'login', changefreq },
+    ];
+
+    // Additionally, you can do database query and add more dynamic URLs to the "links" array.
+
+    const stream = new SitemapStream({ hostname: 'https://www.lamur.us', lastmodDateOnly: true })
+    return streamToPromise(Readable.from(links).pipe(stream)).then((data) => {
+      sitemap = data; // Cache the generated sitemap
+      stream.end();
+      return res.status(200).send(data.toString())
+    });
+
+  } catch (error) {
+    return res.status(500).end();
+  }
+}
+
+module.exports = { generate_sitemap };
