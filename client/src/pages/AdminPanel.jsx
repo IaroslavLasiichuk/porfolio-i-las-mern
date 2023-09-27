36c5f36a763806019835cfebbc17ff5a6cb2115f
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-09-27 10:32:01 -0600

    SEO for all pages

--- client/src/pages/AdminPanel.jsx
+++ client/src/pages/AdminPanel.jsx
@@ -1,10 +1,16 @@
 import Navbar from "../components/Navbar";
 import Admin from "../components/Admin";
 import Footer from "../components/Footer";
+import SEO from './SEO';
 
 const AdminPanel = () => {
   return (
     <>
+       <SEO
+        title='Iaroslav Lasiichuk Portfolio || Website'
+        description='Hi, everyone! I am a young aspiring Full Stack Web developer with a passion for technology and a strong desire to make a difference in the world'
+        canonical='https://www.lamur.us'
+      />
       <Navbar />
       <Admin />
       <Footer />
--- client/src/pages/Registration.jsx
+++ client/src/pages/Registration.jsx
@@ -1,9 +1,15 @@
 import Navbar from "../components/Navbar";
 import Signup from "../components/Signup";
+import SEO from "./SEO";
 
 const Registration = () => {
   return (
     <>
+      <SEO
+        title="Iaroslav Lasiichuk Portfolio || Website"
+        description="Hi, everyone! I am a young aspiring Full Stack Web developer with a passion for technology and a strong desire to make a difference in the world"
+        canonical="https://www.lamur.us"
+      />
       <Navbar />
       <Signup />
     </>
