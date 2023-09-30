6a04117b45989668b32d961fc72f30c8659eba49
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-09-30 13:08:03 -0600

    Adds new feature for reset Password

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -1,17 +1,15 @@
 import Navbar from './Navbar'
 import About from './About.jsx'
 import Footer from './Footer.jsx'
-import { Outlet } from 'react-router-dom'
 
-const Layout = () => {
+const ForgotPassword= () => {
   return (
-    <div className="layout">
+    <>
       <Navbar />
       <About />
       <Footer />
-      <Outlet />
-    </div>
+    </>
   );
 };
 
-export default Layout;
+export default ForgotPassword;
