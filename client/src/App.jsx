a6afd25334a6fcece3750a5e700aed492c4753c3
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-09-30 13:19:37 -0600

    New page for reset created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Contact from './pages/Contact'
 import AdminPanel from './pages/AdminPanel'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
+import ForgotPassword from './pages/ForgotPassword'
 import Registration from './pages/Registration'
 import NotFound from './pages/NotFound'
 
@@ -56,6 +57,7 @@ function App() {
         <Route path="dashboard" element={<Dashboard/>} />
         <Route path="login" element={<Login/>} />
         <Route path="registration" element={<Registration/>} />
+        <Route path="forgotpassword" element={<ForgotPassword/>} />
         <Route path="*" element={<NotFound/>} />
       </Routes>
       </ApolloProvider>
